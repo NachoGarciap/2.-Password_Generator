2df85bce4f72f9c3d375fbcfe26f4bc98c5a683d
NachoGarcia <ignaciogarcia.dev@gmail.com>
2025-01-28 22:05:17 +0100

    mejoras del programa introducidas

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 2.-Password Generator
-2. Generador de Constraseñas
+Este proyecto es un generador de contraseñas seguras en Python que permite a los usuarios crear contraseñas aleatorias de diferentes niveles de seguridad. Puedes elegir entre varios niveles de dificultad y especificar el número de caracteres para la contraseña generada.
+
+# Características
+Generación de contraseñas aleatorias según 3 niveles de seguridad:
+Nivel 1: Solo letras mayúsculas y minúsculas.
+Nivel 2: Letras mayúsculas, minúsculas y números.
+Nivel 3: Letras mayúsculas, minúsculas, números y símbolos especiales.
+El usuario puede especificar la longitud de la contraseña deseada.
+El programa valida las entradas del usuario para asegurar que se ingresen datos correctos.
---
+++ password generator/niveles.py
@@ -0,0 +1,58 @@
+import random
+import string
+import sys
+
+
+class GeneradorContraseñas:
+
+    def __init__(self):
+        # Definir los conjuntos de caracteres
+        self.letras_mayus = string.ascii_uppercase  # "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.letras_minus = string.ascii_lowercase  # "abcdefghijklmnopqrstuvwxyz"
+        self.numeros = string.digits  # "0123456789"
+        self.simbolos = string.punctuation  # "!@#$%^&*()_+[]{},.<>?/|"
+
+        # Diccionario de niveles
+
+        self.niveles = {
+            1: self.letras_mayus + self.letras_minus,  # Nivel 1: Solo letras
+            2: self.letras_mayus + self.letras_minus + self.numeros,  # Nivel 2: Letras + Números
+            3: self.letras_mayus + self.letras_minus + self.numeros + self.simbolos
+            # Nivel 3: Letras + Números + Símbolos
+        }
+
+    def generar_contraseña(self, nivel, num_caracteres):
+        if nivel not in self.niveles: #si el nivel no esta en el diccionario de niveles
+            print("Nivel no válido")
+            return None
+        else:
+            caracteres = self.niveles[nivel]
+            return ''.join(random.choices(caracteres, k=num_caracteres))
+
+    def menu(self):
+        while True:
+            print('-----Generador de contraseñas-----')
+            print('1. Crear contraseña')
+            print('2. Salir del programa')
+
+            try:
+                opcion = int(input('Elige una opción del 1 al 2:'))
+
+                if opcion == 1:
+                    nivel = int(input('Elige un nivel de dificultad (1, 2, 3): '))
+                    num_caracteres = int(input('Introduce el número de caracteres: '))
+
+                    if num_caracteres < 5:
+                        print('El número de caracteres debe ser al menos 6.')
+                    else:
+                        contraseña = self.generar_contraseña(nivel, num_caracteres)
+                        print(f'Contraseña generada: {contraseña}')
+
+                elif opcion == 2:
+                    sys.exit('Saliendo del programa...')
+
+                else:
+                    print("Opción no válida. Intenta nuevamente.")
+
+            except Exception as e:
+                print(f'Error: {e}. Debes ingresar un número válido!')
